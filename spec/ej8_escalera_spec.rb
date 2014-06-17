8b4a310323d25094e8058189fce47f54526c4efd
Pablo Oldani <oldani.pablo@gmail.com>
2014-06-17 10:36:18 -0300

    Agrego el ejercicio de la escalera.

---
+++ spec/ej8_escalera_spec.rb
@@ -0,0 +1,30 @@
+# no te preocupes por esta linea, más tarde la vas a entender
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+begin
+  require 'escalera'
+rescue LoadError
+  puts <<-INSTRUCTIONS
+    Dado un numero n, retornar un array con los valores de 1 a n
+    Avanzado: no usar iteradores.
+
+
+INSTRUCTIONS
+  raise
+end
+
+describe '#triangulo' do
+
+  it 'escalera a 4' do
+    expect(escalera 4).to eq [1, 2, 3, 4]
+  end
+
+  it 'escalera a 5' do
+    expect(escalera 5).to eq [1, 2, 3, 4, 5]
+  end
+
+  it 'escalera a 6' do
+    expect(escalera 5).to eq [1, 2, 3, 4, 5, 6]
+  end
+
+end
